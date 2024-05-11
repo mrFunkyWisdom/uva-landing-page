8d9c95c5bd225557b78baec728253bfab54af2ad
mrFunkyWisdom <ensar.bavrk@gmail.com>
2024-05-12 00:11:39 +0200

    remove unused function from docs

--- app/(docs)/guides/[...slug]/page.tsx
+++ app/(docs)/guides/[...slug]/page.tsx
@@ -9,11 +9,9 @@ import { DocsPageHeader } from "@/components/page-header"
 import { DashboardTableOfContents } from "@/components/toc"
 
 import "@/styles/mdx.css"
-import { Metadata } from "next"
 
-import { absoluteUrl, cn } from "@/lib/utils"
+import {  cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import {siteConfig} from "@/config/site";
 
 interface GuidePageProps {
   params: {
@@ -30,56 +28,6 @@ async function getGuideFromParams(params) {
   }
 
   return guide
-}
-
-export async function generateMetadata({
-  params,
-}: GuidePageProps): Promise<Metadata> {
-  const guide = await getGuideFromParams(params)
-
-  if (!guide) {
-    return {}
-  }
-
-  const url = siteConfig.url
-
-  const ogUrl = new URL(`${url}/api/og`)
-  ogUrl.searchParams.set("heading", guide.title)
-  ogUrl.searchParams.set("type", "Guide")
-  ogUrl.searchParams.set("mode", "dark")
-
-  return {
-    title: guide.title,
-    description: guide.description,
-    openGraph: {
-      title: guide.title,
-      description: guide.description,
-      type: "article",
-      url: absoluteUrl(guide.slug),
-      images: [
-        {
-          url: ogUrl.toString(),
-          width: 1200,
-          height: 630,
-          alt: guide.title,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: guide.title,
-      description: guide.description,
-      images: [ogUrl.toString()],
-    },
-  }
-}
-
-export async function generateStaticParams(): Promise<
-  GuidePageProps["params"][]
-> {
-  return allGuides.map((guide) => ({
-    slug: guide.slugAsParams.split("/"),
-  }))
 }
 
 export default async function GuidePage({ params }: GuidePageProps) {
